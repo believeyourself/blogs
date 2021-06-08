f952b5d3755551b41911ef1f5ad9c615ca026a0e
unknown <634206380@qq.com>
2021-06-08 16:03:44 +0800

    内容补充

--- front/data/data.ts
+++ front/data/data.ts
@@ -146,36 +146,263 @@ export default [
   {
     id: "community",
     category: "技术社区",
-    sites: [],
+    sites: [
+      {
+        title: "GitHub",
+        description:
+          "星球上最流行的基于git的托管服务，越来越多知名开源项目迁入。",
+        url: "https://github.com/",
+      },
+      {
+        title: "Stack Overflow",
+        description: "国外技术问答，开发人员学习、分享和建立职业生涯的地方。",
+        url: "https://stackoverflow.com/",
+      },
+      {
+        title: "掘金",
+        description:
+          "一个帮助开发者成长的社区，技术文章由技术大牛和极客共同编辑筛选出最优质的干货，包括：Android、iOS、前端、后端等方面的内容。",
+        url: "https://juejin.cn/",
+      },
+      {
+        title: "SegmentFault",
+        description:
+          "国内技术问答，为开发者提供问答、学习与交流编程知识的平台，创造属于开发者的时代！",
+        url: "https://segmentfault.com/",
+      },
+      {
+        title: "CNode",
+        description: "Node.js专业中文社区",
+        url: "https://cnodejs.org/",
+      },
+      {
+        title: "博客园",
+        description:
+          "一直致力并专注于为开发者打造一个纯净的技术交流社区，推动并帮助开发者通过互联网分享知识，从而让更多开发者从中受益。",
+        url: "https://www.cnblogs.com/",
+      },
+      {
+        title: "OSCHINA",
+        description:
+          "是目前领先的中文开源技术社区。我们传播开源的理念，推广开源项目，为 IT 开发者提供了一个发现、使用、并交流开源技术的平台",
+        url: "https://www.oschina.net/",
+      },
+      {
+        title: "V2ex",
+        description: "是一个关于分享和探索的地方",
+        url: "https://www.v2ex.com/",
+      },
+      {
+        title: "微信开放社区",
+        description: "官方小程序、小游戏、微信支付交流社区",
+        url: "https://developers.weixin.qq.com/community/homepage",
+      },
+      {
+        title: "蓝色理想",
+        description:
+          "以建设网站设计与开发人员之家为宗旨，以介绍网络开发技术与网站创作设计交流为主要内容。",
+        url: "http://www.blueidea.com/",
+      },
+    ],
   },
   {
     id: "frame",
     category: "框架类库",
-    sites: [],
+    sites: [
+      {
+        title: "React",
+        description: "用于构建用户界面的 JavaScript 库",
+        url: "https://zh-hans.reactjs.org/",
+      },
+      {
+        title: "Vue",
+        description: "渐进式 JavaScript 框架",
+        url: "https://cn.vuejs.org/",
+      },
+      {
+        title: "AngularJS",
+        description: "一套框架，多种平台。移动端 & 桌面端",
+        url: "https://angular.cn/",
+      },
+    ],
   },
   {
     id: "animation",
     category: "图形动效",
-    sites: [],
+    sites: [
+      {
+        title: "Animate.css",
+        description:
+          "是一个使用CSS3的animation制作的动画效果的CSS集合，里面预设了很多种常用的动画，且使用非常简单。",
+        url: "https://github.com/animate-css/animate.css",
+      },
+      {
+        title: "GreenSock",
+        description: "超高性能、专业级的现代网络动画",
+        url: "https://github.com/greensock/GSAP",
+      },
+    ],
   },
   {
     id: "game",
     category: "游戏开发",
-    sites: [],
+    sites: [
+      {
+        title: "PixiJS",
+        description: "使用最快，最灵活的2D WebGL渲染器创建精美的数字内容。",
+        url: "https://www.pixijs.com/",
+      },
+      {
+        title: "Cocos Creator",
+        description:
+          "以内容创作为核心，实现了脚本化、组件化和数据驱动的游戏开发工具。",
+        url: "https://docs.cocos.com/creator/manual/zh/",
+      },
+      {
+        title: "Egret",
+        description:
+          "效率至上的一站式完整开发工作流，2D/3D双核心渲染，开发工具链闭环整合，全流程开发，由此开始",
+        url: "https://www.egret.com/",
+      },
+    ],
   },
   {
     id: "weekly",
     category: "前端周刊",
-    sites: [],
+    sites: [
+      {
+        title: "阿里云前端技术周刊",
+        description:
+          "由阿里云智能商业中台体验技术团队整理编写，希望能给社区带来更多有价值的内容，迎接云上的大前端时代！",
+        url: "https://github.com/aliyunfe/weekly",
+      },
+      {
+        title: "FEX 技术周刊",
+        description: "会推送优秀的前端项目和不错的技术文章。",
+        url: "http://fex.baidu.com/weekly/",
+      },
+      {
+        title: "奇舞周刊",
+        description:
+          "领略前端技术 阅读奇舞周刊。收集每周前端精华文章，集结成册。",
+        url: "https://weekly.75.team/",
+      },
+      {
+        title: "前端精读周刊",
+        description: "前端界的好文精读，每周更新！",
+        url: "https://github.com/ascoders/weekly",
+      },
+      {
+        title: "JS 周刊",
+        description:
+          "JavaScript Weekly 是一个收集 JavaScript 新闻和文章的周刊。",
+        url: "https://docschina.org/weekly/js/docs/",
+      },
+      {
+        title: "TNFE-Weekly",
+        description:
+          "腾讯新闻前端团队-TNFE每周为你提供高质量的关于小程序、h5等前端领域的文章和项目",
+        url: "https://github.com/tnfe/TNT-Weekly",
+      },
+    ],
+  },
+  {
+    id: "interview",
+    category: "笔试面试",
+    sites: [
+      {
+        title: "前端面试每日三问",
+        description:
+          "前端面试每日 3+1，以面试题来驱动学习，提倡每日学习与思考，每天进步一点！",
+        url: "https://github.com/haizlin/fe-interview",
+      },
+    ],
   },
   {
     id: "debugTool",
     category: "调试工具",
-    sites: [],
+    sites: [
+      {
+        title: "Fiddler",
+        description:
+          "免费的Web调试代理，对于任何浏览器、系统或平台。windows首选。",
+        url: "https://www.telerik.com/fiddler",
+      },
+      {
+        title: "Charles",
+        description: "Web调试代理，mac首选。",
+        url: "https://www.charlesproxy.com/",
+      },
+      {
+        title: "whistle",
+        description:
+          "基于Node实现的跨平台抓包调试代理工具，覆盖了所有抓包调试代理可以实现的功能",
+        url: "https://github.com/avwo/whistle/blob/master/README-zh_CN.md",
+      },
+      {
+        title: "Postman",
+        description:
+          "优秀的，完善的接口测试工具。能够高效的帮助后端开发人员独立进行接口测试。",
+        url: "https://www.postman.com/",
+      },
+      {
+        title: "Browsersync",
+        description:
+          "省时的浏览器同步测试工具，可以同时在PC、平板、手机等设备下进项调试。",
+        url: "https://browsersync.io/",
+      },
+      {
+        title: "vConsole",
+        description: "一个轻量、可拓展、针对手机网页的前端开发者调试面板。",
+        url: "https://github.com/Tencent/vConsole/blob/dev/README_CN.md",
+      },
+      {
+        title: "eruda",
+        description:
+          "一个专为手机网页前端设计的调试面板，类似 DevTools 的迷你版，包括 console、检查元素状态、XHR请求、显示本地存储和 Cookie 等。",
+        url: "https://github.com/liriliri/eruda/blob/master/doc/README_CN.md",
+      },
+      {
+        title: "JS Bin",
+        description:
+          "协同web开发调试工具，可以快速的提供前端需要的各种开发环境，快速地编辑代码，并实时显示页面最终的效果。",
+        url: "https://jsbin.com/?html,output",
+      },
+    ],
   },
   {
     id: "buildTool",
     category: "构建工具",
-    sites: [],
+    sites: [
+      {
+        title: "webpack",
+        description:
+          "webpack 是一个现代 JavaScript 应用程序的静态模块打包器(module bundler)。",
+        url: "https://webpack.github.io/",
+      },
+      {
+        title: "vite",
+        description: "下一代前端开发与构建工具",
+        url: "https://cn.vitejs.dev/",
+      },
+      {
+        title: "FIS3",
+        description:
+          "解决前端开发中自动化工具、性能优化、模块化框架、开发规范、代码部署、开发流程等问题",
+        url: "http://fis.baidu.com/",
+      },
+      {
+        title: "Gulp",
+        description:
+          "基于流的自动化构建工具，可以快速构建项目并减少频繁的 IO 操作。",
+        url: "https://www.gulpjs.com.cn/",
+      },
+      {
+        title: "Grunt",
+        description:
+          "JavaScript 世界的构建工具，印象里最早出现，现在用的少了。",
+        url: "https://gruntjs.com/",
+      },
+    ],
   },
 ];
