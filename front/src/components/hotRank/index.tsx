30868cd7ab02905c9cd60abbd6e8916999786ed0
lzj <634206380@qq.com>
2021-07-03 12:54:30 +0800

    热门排行

--- front/src/components/hotRank/index.tsx
+++ front/src/components/hotRank/index.tsx
@@ -1,9 +1,22 @@
 import React from "react";
 import style from "./index.less";
+import blogs from "../../../data/blogs";
+
+const rank = blogs.slice(blogs.length - 5, blogs.length);
 
 function HotRank () {
+  const listItems = rank.map((item: any, index:number) => {
+    return (
+      <a href={item.url} target="_blank" key={item.url} className={style.rank_item} rel="noreferrer">
+        {index + 1}.{item.title}
+      </a>
+    );
+  });
   return <div className={style.container}>
-111
+      <p className={style.title}>热门排行</p>
+      <ul className={style.rank_list}>
+        {listItems}
+      </ul>
   </div>;
 }
 
