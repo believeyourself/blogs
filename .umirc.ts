ec8ee65439283f34f1f057ad083942c1b8c4e2a5
lzj <634206380@qq.com>
2021-09-17 22:04:52 +0800

    文章详情

--- .umirc.ts
+++ .umirc.ts
@@ -6,35 +6,40 @@ export default defineConfig({
   },
   routes: [
     {
-      path:"/",
-      component:"@/layouts",
-      routes:[
-        { 
-          path: '/', 
-          redirect: '/sites'
+      path: '/',
+      component: '@/layouts',
+      routes: [
+        {
+          path: '/',
+          redirect: '/sites',
         },
-        { 
-          path: '/sites', 
-          title:"网址导航",
-          component: '@/pages/sites'
+        {
+          path: '/sites',
+          title: '网址导航',
+          component: '@/pages/sites',
         },
-        { 
-          title:"教程资源",
-          path: '/resources', 
-          component: '@/pages/resources'
+        {
+          title: '教程资源',
+          path: '/resources',
+          component: '@/pages/resources',
         },
-        { 
-          title:"优质好文",
-          path: '/blogs', 
-          component: '@/pages/blogs'
+        {
+          title: '优质好文',
+          path: '/blogs',
+          component: '@/pages/blogs',
         },
-        { 
-          title:"关于我们",
-          path: '/aboutUs', 
-          component: '@/pages/aboutUs'
+        {
+          title: '文章详情',
+          path: '/blogs/detail/:id',
+          component: '@/pages/blogs/detail/detail',
         },
-      ]
-    }
+        {
+          title: '关于我们',
+          path: '/aboutUs',
+          component: '@/pages/aboutUs',
+        },
+      ],
+    },
   ],
   fastRefresh: {},
   ssr: {},
