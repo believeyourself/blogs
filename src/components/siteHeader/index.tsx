dd38d8282d20494532336ad9e5f2e4fd064d1ae1
HeZhihao <ylwshzh@163.com>
2021-09-17 16:10:02 +0800

    引入adSense

--- src/components/siteHeader/index.tsx
+++ src/components/siteHeader/index.tsx
@@ -6,10 +6,13 @@ export interface SiteHeaderProps {
 }
 
 export default ({description,keywords}:SiteHeaderProps)=>{
-  console.log("111",process.env.REACT_APP_ENV)
   return <Helmet>
       <meta name="description" content={description || "前端网，免费前端优质资源、教程"} />
       <meta name="keywords" content={keywords||"HTML,CSS,JavaScript,React,Vue,NodeJs,前端,免费,教程,源码,开源项目"} />
       <meta name="robots" content="nofollow" />
+      {
+      process.env.REACT_APP_ENV === "production" &&
+      <script data-ad-client="ca-pub-2087076429727327" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+      }
   </Helmet>
 }
