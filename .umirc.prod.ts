98044ea58e52f8837541f54255055bfe10b95ac8
HeZhihao <ylwshzh@163.com>
2021-09-18 09:35:51 +0800

    百度统计

--- .umirc.prod.ts
+++ .umirc.prod.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'umi';
 export default defineConfig({
   define:{
     "process.env.REACT_APP_ENV": "production"
-  }
+  },
+  analytics: {
+    baidu: '926fb44514b1d81a809cb5380bf10511',
+  },
 });
