d6c181cd3d3711ac0d6202b2557065e7155688fa
HeZhihao <ylwshzh@163.com>
2021-10-20 15:32:13 +0800

    备案链接防止爬取

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -3,7 +3,7 @@ export default () => {
   return (
     <footer className={style.footer}>
       ©2021 farmerlzj 提供技术支持 | 联系我们：farmerlzj@163.com |{' '}
-      <a href="https://beian.miit.gov.cn/" target="_blank">
+      <a href="https://beian.miit.gov.cn/" rel="nofollow" target="_blank">
         蜀ICP备2021025378号-1
       </a>
     </footer>
