1b5de008e5726896c9493584dab71d2b4f5ed6fd
lzj <634206380@qq.com>
2021-11-03 22:39:42 +0800

    首页UI修改

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -12,51 +12,61 @@ import resource from '../../../data/resource';
 import NavLink from '@/components/navLink';
 import Card from '@/components/card';
 import SiteHeader from '@/components/siteHeader';
-import { Divider,Card as AntCard,Row,Col } from 'antd';
+import { Divider, Card as AntCard, Row, Col } from 'antd';
 
-const someBlogs = blogs.slice(0,10);
-const someProjects = resource[1].list.slice(0,4);
+const someBlogs = blogs.slice(0, 10);
+const someProjects = resource[1].list.slice(0, 4);
 const blogRecords = someBlogs.map((item: any) => {
-    if (item.id) {
-      return (
-        <NavLink
-          to={`/blogs/detail/${item.id}`}
-          key={item.url || item.id}
-          className={styles.category_list_item}
-        >
-          <h4 className={styles.title}>{item.title}</h4>
-          <p className={styles.description}>{item.description}</p>
-          <p className={styles.tip}>
-            来源: 本站 {'           '}
-            时间: {item.date}
-          </p>
-        </NavLink>
-      );
-    } else {
-      return (
-        <a
-          href={item.url}
-          target="_blank"
-          key={item.url || item.id}
-          className={styles.category_list_item}
-          rel="nofollow"
-        >
-          <h4 className={styles.title}>{item.title}</h4>
-          <p className={styles.description}>{item.description}</p>
-          <p className={styles.tip}>
-            来源: {item.source} {'          '}
-            时间: 未知
-          </p>
-        </a>
-      );
-    }
-  });
+  if (item.id) {
+    return (
+      <NavLink
+        to={`/blogs/detail/${item.id}`}
+        key={item.url || item.id}
+        className={styles.category_list_item}
+      >
+        <h4 className={styles.title}>{item.title}</h4>
+        <p className={styles.description}>{item.description}</p>
+        <p className={styles.tip}>
+          来源: 本站 {'           '}
+          时间: {item.date}
+        </p>
+      </NavLink>
+    );
+  } else {
+    return (
+      <a
+        href={item.url}
+        target="_blank"
+        key={item.url || item.id}
+        className={styles.category_list_item}
+        rel="nofollow"
+      >
+        <h4 className={styles.title}>{item.title}</h4>
+        <p className={styles.description}>{item.description}</p>
+        <p className={styles.tip}>
+          来源: {item.source} {'          '}
+          时间: 未知
+        </p>
+      </a>
+    );
+  }
+});
 const projectRecords = someProjects.map((item) => {
-  return <Col key={item.url} md={12} sm={24}>
-    <Card {...item}>
-    </Card>
-  </Col>
-})
+  return (
+    <Col key={item.url} md={12} sm={24}>
+      <Card {...item}></Card>
+    </Col>
+  );
+});
+
+const Title = (props: any) => {
+  return (
+    <h2 className={styles.part}>
+      <span>{props.title}</span>
+      <NavLink to={props?.more}>{props?.description}</NavLink>
+    </h2>
+  );
+};
 export default function Blogs() {
   return (
     <>
@@ -66,28 +76,52 @@ export default function Blogs() {
         description="致力于前端开发领域技术研究，深入分析技术基础原理，教程资源，开源项目，开发工具分享，包括但不限于JavaScript,css,React,vue,node,webpack,umi,ant-design,dav"
       />
       <div className={styles.content}>
-        <Divider>常用网址</Divider>
+        <Title title="常用网址" more="/sites" description={'更多网址 >>'} />
         <div className={styles.sites}>
-          <a href="" target="_blank" rel="nofollow">Chrome插件</a>
-          <a href="" target="_blank" rel="nofollow">图片压缩</a>
-          <a href="https://www.tutieshi.com/" target="_blank" rel="nofollow">gif制作</a>
-          <NavLink to="/tools/jsonTree" target="_blank" rel="nofollow">JSON格式转换</NavLink>
-          <a href="https://vjyd1gg9jl.feishu.cn/base/app4Dlc49N1Z9e4LrAoZ36lPMNi?from=from_copylink" target="_blank" rel="nofollow">前端面试真题</a>
-          <a href="https://tool.lu/markdown/" target="_blank" rel="nofollow">在线Markdown</a>
-          <a href="https://cli.im/" target="_blank" rel="nofollow">二维码生成器</a>
-          <a href="https://color.uisdc.com/pick.html" target="_blank" rel="nofollow">配色导航</a>
-          <a href="https://juejin.cn/" target="_blank" rel="nofollow">掘金</a>
-           <NavLink to="/sites">{"更多网址>>"}</NavLink>
+          <a href="" target="_blank" rel="nofollow">
+            Chrome插件
+          </a>
+          <a href="" target="_blank" rel="nofollow">
+            图片压缩
+          </a>
+          <a href="https://www.tutieshi.com/" target="_blank" rel="nofollow">
+            gif制作
+          </a>
+          <NavLink to="/tools/jsonTree" target="_blank" rel="nofollow">
+            JSON格式转换
+          </NavLink>
+          <a
+            href="https://vjyd1gg9jl.feishu.cn/base/app4Dlc49N1Z9e4LrAoZ36lPMNi?from=from_copylink"
+            target="_blank"
+            rel="nofollow"
+          >
+            前端面试真题
+          </a>
+          <a href="https://tool.lu/markdown/" target="_blank" rel="nofollow">
+            在线Markdown
+          </a>
+          <a href="https://cli.im/" target="_blank" rel="nofollow">
+            二维码生成器
+          </a>
+          <a
+            href="https://color.uisdc.com/pick.html"
+            target="_blank"
+            rel="nofollow"
+          >
+            配色导航
+          </a>
+          <a href="https://juejin.cn/" target="_blank" rel="nofollow">
+            掘金
+          </a>
         </div>
-        <Divider>开源项目</Divider>
-        <Row gutter={[10,10]}>
-          {projectRecords}
-        </Row>
-        <div style={{textAlign:"center",padding:20}}>
-          <NavLink to="/resources/originCode">{"更多开源项目 >>"} </NavLink>
-        </div>
-        <Divider>专题汇总</Divider>
-        <div style={{display:"flex",gap:20}}>
+        <Title
+          title="开源项目"
+          more="/resources/originCode"
+          description={'更多项目 >>'}
+        />
+        <Row gutter={[10, 10]}>{projectRecords}</Row>
+        <Title title="专题汇总" />
+        <div style={{ display: 'flex', gap: 20 }}>
           <NavLink className={styles.topic} to={'/topics/react'}>
             React 专题
           </NavLink>
@@ -95,11 +129,8 @@ export default function Blogs() {
             Node 专题
           </NavLink>
         </div>
-        <Divider>文章随笔</Divider>
+        <Title title="博客随笔" more="/blogs" description={'更多文章 >>'} />
         {blogRecords}
-        <div  style={{textAlign:"center"}}>
-          <NavLink to="/blogs">{"更多文章 >>"} </NavLink>
-        </div>
       </div>
     </>
   );
