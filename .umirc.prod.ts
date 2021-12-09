9ccf8e2e4950c86440ae136de3d4415a8a70693f
HeZhihao <ylwshzh@163.com>
2021-12-09 15:33:11 +0800

    update .gitignore

--- .umirc.prod.ts
+++ .umirc.prod.ts
@@ -1,7 +1,7 @@
 /*
  * @Date: 2021-09-22 08:59:01
  * @LastEditors: lzj
- * @LastEditTime: 2021-12-09 15:28:46
+ * @LastEditTime: 2021-12-09 15:31:51
  * @FilePath: \qianduan.shop\.umirc.prod.ts
  */
 import { defineConfig } from 'umi';
@@ -9,7 +9,7 @@ import { defineConfig } from 'umi';
 export default defineConfig({
   define: {
     'process.env.REACT_APP_ENV': 'production',
-    'process.env.SITE_DOMAIN': 'http://localhost:8080',
+    'process.env.SITE_DOMAIN': 'https://qianduan.shop',
   },
   analytics: {
     baidu: '926fb44514b1d81a809cb5380bf10511',
