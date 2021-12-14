499409a4b6f4a9ec173dc3bef8ef8c796c39e1fa
lzj <634206380@qq.com>
2021-12-14 22:15:57 +0800

    ad

--- src/components/squareAd/index.tsx
+++ src/components/squareAd/index.tsx
@@ -1,9 +1,7 @@
-import { Helmet } from 'umi';
 import AdSense from 'react-adsense';
 import ErrorBoundary from '../ErrorBoundary';
 
-export default (props: any) => {
-  const { width = 250, height = 300 } = props;
+export default () => {
   if (process.env.REACT_APP_ENV === 'development') {
     return null;
   }
@@ -14,13 +12,7 @@ export default (props: any) => {
         <AdSense.Google
           client="ca-pub-2087076429727327"
           slot="7022357395"
-          style={{
-            display: 'inline-block',
-            width,
-            maxWidth: width,
-            height,
-            maxHeight: height,
-          }}
+          style="display:inline-block;width:250px;height:300px"
         />
       </div>
     </ErrorBoundary>
